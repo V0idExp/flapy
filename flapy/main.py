8d1df69165498cb59fa9e6b925bfc04e379ffaec
Ivan Nikolaev <voidexp@gmail.com>
2017-04-09 16:41:55 +0200

    Add Game class, declarative game data, rocks

--- flapy/main.py
+++ flapy/main.py
@@ -1,4 +1,7 @@
 """Flapy - a Flappy Bird clone in Python."""
+from abc import ABC
+from abc import abstractmethod
+from abc import abstractproperty
 from itertools import cycle
 import os
 import pygame
@@ -7,8 +10,22 @@ import sys
 # Screen size in pixels.
 SCREEN_SIZE = 800, 480
 
-# Scroll speed in pixels.
-SCROLL_SPEED = 200
+# Game data.
+GAME_DATA = {
+    'scroll_speed': 200,
+    'entities': [
+        {
+            'id': 0,
+            'type': 'rock',
+            'position': (400, 241),
+        },
+        {
+            'id': 1,
+            'type': 'rock',
+            'position': (1910, 241),
+        }
+    ]
+}
 
 
 class ResourceError(FileNotFoundError):
@@ -27,6 +44,62 @@ def load_image(filename):
         return image.convert_alpha()
     except pygame.error as err:
         raise ResourceError(err)
+
+
+class Entity(ABC):
+
+    @abstractproperty
+    def x(self):
+        """Object position X coordinate."""
+
+    @x.setter
+    def x(self, x):
+        """Set object position X coordinate."""
+
+    @abstractproperty
+    def y(self):
+        """Object position Y coordinate."""
+
+    @y.setter
+    def y(self, y):
+        """Set object position Y coordinate."""
+
+    @abstractmethod
+    def update(self, dt):
+        """Updates the entity."""
+
+    @abstractmethod
+    def draw(self, surface):
+        """Draws the entity on given surface."""
+
+
+class Rock(Entity):
+
+    def __init__(self, position):
+        self.image = load_image(resource_filename('/rockGrass.png'))
+        self._x, self._y = position
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, x):
+        self._x = x
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, y):
+        self._y = y
+
+    def draw(self, surface):
+        surface.blit(self.image, (self._x, self._y))
+
+    def update(self, dt):
+        pass
 
 
 class Background:
@@ -97,22 +170,72 @@ class Plane(pygame.sprite.Sprite):
             self.set_frame(next(self.indices))
 
 
+class Game:
+
+    def __init__(self, screen_size, data):
+        self.width, self.height = screen_size
+        self.distance = 0
+        self.data = data
+        self.entities = {}
+        self.background = Background(data['scroll_speed'])
+        self.objects = pygame.sprite.Group()
+
+        self.plane = Plane()
+        self.objects.add(self.plane)
+
+    def spawn_entities(self):
+        for spec in self.data['entities']:
+            if spec['id'] in self.entities:
+                continue
+
+            x, y = spec['position']
+            if x > self.distance and x < self.distance + self.width * 2:
+                if spec['type'] == 'rock':
+                    x -= self.distance
+                    entity = Rock((x, y))
+                    self.entities[spec['id']] = entity
+                    print('Added {} entity with id {}'.format(spec['type'], spec['id']))
+
+    def update_entities(self, dt):
+        for entity in self.entities.values():
+            entity.x -= self.data['scroll_speed'] * dt
+            entity.update(dt)
+
+    def prune_entities(self):
+        to_remove = []
+        for eid, entity in self.entities.items():
+            if entity.x < -self.width:
+                to_remove.append(eid)
+
+        for eid in to_remove:
+            self.entities.pop(eid)
+            print('Removed entity {}'.format(eid))
+
+    def update(self, dt):
+        # update global distance counter
+        self.distance += dt * self.data['scroll_speed']
+
+        self.spawn_entities()
+        self.update_entities(dt)
+        self.prune_entities()
+
+        self.background.update(dt)
+        self.objects.update(dt)
+
+    def draw(self, screen):
+        self.background.draw(screen)
+        self.objects.draw(screen)
+        for entity in self.entities.values():
+            entity.draw(screen)
+
+
 if __name__ == '__main__':
     pygame.init()
 
     screen = pygame.display.set_mode(SCREEN_SIZE)
     pygame.display.set_caption('FlaPy')
 
-    # create background
-    background = Background(SCROLL_SPEED)
-
-    # create plane
-    plane = Plane()
-    plane.x = 30
-    plane.y = SCREEN_SIZE[1] * 0.3
-
-    sprites = pygame.sprite.Group()
-    sprites.add(plane)
+    game = Game(SCREEN_SIZE, GAME_DATA)
 
     last_update = pygame.time.get_ticks()
     while True:
@@ -127,11 +250,9 @@ if __name__ == '__main__':
         last_update = now
 
         # update entities
-        sprites.update(dt)
-        background.update(dt)
+        game.update(dt)
 
         # render
         screen.fill((0, 0, 0))
-        background.draw(screen)
-        sprites.draw(screen)
+        game.draw(screen)
         pygame.display.flip()
